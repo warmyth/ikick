c60d6caaafaf713241d332adfa8da8d8680bf00b
nico83110 <nico83110@gmail.com>
2013-01-23 12:49:27 +0000

    first commit

---
+++ FunctionManager.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Magic;
+
+namespace iKick
+{
+    public class FunctionManager
+    {
+        private BlackMagic process;
+        private HookManager aHook;
+
+        public FunctionManager(BlackMagic process)
+        {
+            this.process = process;
+            this.aHook = new HookManager(process);
+        }
+
+        public void LuaDoString(string command)
+        {
+            int nSize = command.Length + 0x100;
+            uint codeCave = process.AllocateMemory(nSize);
+            uint moduleBase = (uint)process.MainModule.BaseAddress;
+
+            process.WriteASCIIString(codeCave, command);
+
+            process.Asm.Clear();
+
+            String[] asm = new String[] 
+            {
+                "mov eax, " + codeCave,
+                "push 0",
+                "push eax",
+              
+                "push eax",
+                "mov eax, " + (moduleBase + Offsets.Endscene.Lua_DoStringAddress),
+                
+                "call eax",
+                "add esp, 0xC",
+                "retn",    
+            };
+
+            aHook.InjectAndExecute(asm);
+            process.FreeMemory(codeCave);
+        }
+        public string GetLocalizedText(string command)
+        {
+            int nSize = command.Length + 0x100;
+            uint codeCave = process.AllocateMemory(nSize);
+            uint moduleBase = (uint)process.MainModule.BaseAddress;
+            var ClntObjMgrGetActivePlayerObj = moduleBase + Offsets.Endscene.ClntObjMgrGetActivePlayerObjAddress;
+            var FrameScript__GetLocalizedText = moduleBase + Offsets.Endscene.Lua_GetLocalizedTextAddress;
+
+            process.WriteASCIIString(codeCave, command);
+
+            String[] asm = new String[] 
+                {
+                "call " + ClntObjMgrGetActivePlayerObj,
+                "mov ecx, eax",
+                "push -1",
+                "mov edx, " + codeCave + "",
+                "push edx",
+                "call " + FrameScript__GetLocalizedText,
+                "retn",
+                };
+
+            string sResult = Encoding.ASCII.GetString(aHook.InjectAndExecute(asm));
+            process.FreeMemory(codeCave);
+
+            return sResult;
+        }
+        public void InteractGameObject(uint baseAddress)
+        {
+            uint InteractVMT = 45;
+            if (baseAddress > 0)
+            {
+                uint VMT44 = process.ReadUInt(process.ReadUInt(baseAddress) + ((uint)InteractVMT * 4));
+                var objectManagerBase = 0x463C;
+
+                string[] asm = new string[]
+                {
+            "fs mov eax, [0x2C]",
+            "mov eax, [eax]",
+            "add eax, 0x10",
+            "mov dword [eax], " + objectManagerBase,
+            "mov ecx, " + baseAddress,
+            "call " + VMT44,
+            "retn",
+                 };
+
+
+                aHook.InjectAndExecute(asm);
+            }
+        }
+    }
+
+}
---
+++ Offsets.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace iKick
+{
+    class Offsets
+    {
+        public struct Direct3D
+        {
+            public static uint Direct3D9__Device = 0xAD743C;
+            public static uint Direct3D9__Device__OffsetA = 0x27F8;
+            public static uint Direct3D9__Device__OffsetB = 0xA8;
+        }
+        public enum Endscene : uint
+        {
+            ClntObjMgrGetActivePlayerObjAddress = 0x3390,
+            Lua_DoStringAddress = 0x755A0,
+            Lua_GetLocalizedTextAddress = 0x48EBC0,
+        }
+        public enum afk
+        {
+            LastHardwareAction = 0x9D3798,
+            TimeStamp = 0x9C0B14,
+        }
+    }
+}
